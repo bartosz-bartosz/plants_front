f6cc65818d26939e95935f232276cb672810a4ea
bartosz <badalecki@gmail.com>
2023-07-16 20:16:41 +0200

    checking API token expiration date

--- src/components/PlantCards.vue
+++ src/components/PlantCards.vue
@@ -3,6 +3,7 @@
   import "@fontsource/montserrat"
 
   import axios from "axios";
+  import jwt_decode from 'jwt-decode';
   import { ref, watch, onMounted} from "vue";
   import Card from "@/components/Card.vue";
 
@@ -12,19 +13,38 @@
 
 
   // Clear localStorage to debug acquiring token
-  // localStorage.clear()
+  localStorage.clear()
 
   const customRequest = axios.create({
       baseURL: apiURL,
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
   });
 
+  const isTokenExpired = (token) => {
+  try {
+      const tokenData = jwt_decode(token);
+      const expirationTime = tokenData.exp * 1000; // Convert to milliseconds
+      console.log('Checking if token expired')
+
+      // Compare the expiration time with the current time
+      const currentTime = Date.now();
+      console.log(`current time: ${currentTime}`);
+      console.log(`expiration time: ${expirationTime}`);
+      return currentTime >= expirationTime;
+  } catch (error) {
+      // If there's an error decoding the token or the "exp" claim is missing, consider the token expired
+      console.error('Error decoding token:', error);
+      return true;
+  }
+};
+
   const getToken = async () => {
+      console.log('getToken function called.');
       const data = new URLSearchParams();
       data.append('grant_type', '');
       data.append('username', username);
       data.append('password', password);
-      console.log('Performing token request...')
+      console.log('Performing token request...');
 
       try {
           const response = await customRequest.post('/token', data);
@@ -40,7 +60,8 @@
   const fetchData = async (endpoint) => {
       try {
           let token = localStorage.getItem('accessToken');
-          if (!token) {
+
+          if (!token || isTokenExpired(token)) {
               token = await getToken();
           }
           const config = {
@@ -60,8 +81,8 @@
   let pagination = ref(0)
 
   onMounted(async () =>{
-      console.log('mounted')
-      const plants_res = await fetchData(apiURL+`plant?limit=4&skip=${pagination.value*4}`)
+      console.log('mounted');
+      const plants_res = await fetchData(apiURL+`plant?limit=4&skip=${pagination.value*4}`);
       plants.value = plants_res.data
   });
 
