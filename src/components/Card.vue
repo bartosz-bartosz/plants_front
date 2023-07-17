2452f76f4c950f30212e6c07a5e5770b908cfeec
bartosz <badalecki@gmail.com>
2023-07-17 20:53:40 +0200

    datetime handling

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import {defineProps} from "vue";
+  import { defineProps, ref, watchEffect } from "vue";
 
   const {name, species, watering_frequency, last_watering} = defineProps([
       'name',
@@ -8,7 +8,34 @@
       'last_watering'
   ])
 
+  const localLastWatering = ref('');
+
+    // Watch for changes in the last_watering prop
+  watchEffect(() => {
+    const dateObj = new Date(last_watering);
+    localLastWatering.value = dateObj.toLocaleString();
+  });
+
+    // Computed property to calculate the time since last watering
+  const timeSinceLastWatering = ref('');
+  watchEffect(() => {
+    if (last_watering) {
+      const lastWateringDate = new Date(last_watering);
+      const currentDate = new Date();
+      const timeDifferenceMs = currentDate - lastWateringDate;
+      const daysPassed = Math.floor(timeDifferenceMs / (1000 * 60 * 60 * 24));
+
+      if (daysPassed >= 8) {
+        const weeksPassed = Math.floor(daysPassed / 7);
+        timeSinceLastWatering.value = `${weeksPassed} weeks ago`;
+      } else {
+        timeSinceLastWatering.value = `${daysPassed} days ago`;
+      }
+    }
+  });
+
 </script>
+
 
 <template>
   <div class="card">
@@ -17,7 +44,9 @@
         <div class="line"></div>
         <h3 class="species-italic">{{ species }}</h3>
       </div>
-      <p class="last-watering-date">{{ last_watering }}</p>
+      <p v-if='last_watering' class="last-watering-date">{{ localLastWatering }}</p>
+      <p v-else class="last-watering-date"> This plant hasn't been watered yet :( </p>
+      <p v-if="last_watering">{{ timeSinceLastWatering }}</p>
       <p class="watering-fq-text">Water every <b>{{watering_frequency}}</b> days</p>
   </div>
 </template>
