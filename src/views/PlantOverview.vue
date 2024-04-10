3796407cbd3896c64f304b318949927442448233
bartosz <badalecki@gmail.com>
2024-04-10 23:15:21 +0200

    plant overview in progress

--- src/views/PlantOverview.vue
+++ src/views/PlantOverview.vue
@@ -19,12 +19,32 @@ onMounted(async () => {
     <div>{{ $route.params.id }}
     </div>
   <div class="plant-data" v-if="plantData">
+    <div class="plant-heading">
+      <h1>{{plantData.name}}</h1>
+      <p>YOUR PLANT</p>
+    </div>
+    <div class="plant-stats">
+      <div class="base-data">
+        <div class="data-row">
+          <span class="icon"><font-awesome-icon icon="stopwatch"/>
+          <p>Watering Frequency</p>
+          </span>
+          <p>{{plantData.watering_frequency}} days</p>
+        </div>
+        <div class="data-row">
+          <span class="icon"><font-awesome-icon icon="calendar-day"/>
+          <p>Watering Frequency</p>
+          </span>
+          <p>{{plantData.next_watering}} days</p>
+        </div>
+      </div>
+      <div class="chart">
+        <WateringHistoryChart/>
+      </div>
+    </div>
     {{plantData.name}}
     {{plantData.watering_frequency}}
     {{plantData.last_watering}}
-  </div>
-    <div class="chart">
-      <WateringHistoryChart/>
   </div>
   </body>
 </template>
@@ -32,7 +52,72 @@ onMounted(async () => {
 <style scoped>
 .main {
   border: 1px solid red;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
 }
 
+.plant-data {
+  border: 1px solid blue;
+  margin: auto;
+  width: 90%;
+  justify-content: left;
+}
+
+.plant-heading {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: top;
+  border: 1px solid green;
+  padding-left: 20px;
+}
+
+h1 {
+  text-align: left;
+  align-self: stretch;
+  margin: 0;
+}
+
+p {
+  text-align: left;
+  align-self: stretch;
+  margin: 0;
+}
+
+.plant-stats {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border: 1px solid cyan;
+  padding-left: 20px;
+  min-height: 100px;
+}
+
+.base-data {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 10px;
+}
+.data-row {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  min-width: 240px;
+  margin: 5px;
+
+}
+
+.icon {
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  color: #011b10;
+  font-weight: bold;
+}
 
 </style>
