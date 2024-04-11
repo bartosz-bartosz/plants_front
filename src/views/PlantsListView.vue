f8f6beade2732d300986156ee8dcadc25d520cc8
bartosz <badalecki@gmail.com>
2024-04-11 20:57:52 +0200

    sorting plants

--- src/views/PlantsListView.vue
+++ src/views/PlantsListView.vue
@@ -11,8 +11,8 @@ const userStore = useUserStore()
 
 let listview = ref(true);
 let itemsLimit = ref(24)
-let sort_by = ref("last_watering")
-let sort_order = ref("desc")
+let sort_by = ref("next_watering")
+let sort_order = ref(false)
 const plants = ref(null)
 const plants_count = ref(0)
 let pagination = ref(0)
@@ -23,17 +23,23 @@ onMounted(async () => {
     const plants_count_res = await fetchData(apiURL + `plant/count`)
     plants_count.value = plants_count_res.data.count
 
-    const plants_res = await fetchData(apiURL + `plant/list?limit=${itemsLimit.value}&skip=${pagination.value * itemsLimit.value}&sort_by=${sort_by.value}&sort_order=${sort_order.value}`);
+    const plants_res = await fetchData(apiURL + `plant/list?limit=${itemsLimit.value}&skip=${pagination.value * itemsLimit.value}&sort_by=${sort_by.value}&sort_order=${sort_order.value ? 'asc' : 'desc'}`);
     plants.value = plants_res.data
   }
 
 });
 
 watch(pagination, async () => {
-  const plants_res = await fetchData(apiURL + `plant/list?limit=${itemsLimit.value}&skip=${pagination.value * itemsLimit.value}&sort_by=${sort_by.value}&sort_order=${sort_order.value}`);
+  const plants_res = await fetchData(apiURL + `plant/list?limit=${itemsLimit.value}&skip=${pagination.value * itemsLimit.value}&sort_by=${sort_by.value}&sort_order=${sort_order.value ? 'asc' : 'desc'}`);
   plants.value = plants_res.data
 });
 
+
+function refreshPlants() {
+  fetchData(apiURL + `plant/list?limit=${itemsLimit.value}&skip=${pagination.value * itemsLimit.value}&sort_by=${sort_by.value}&sort_order=${sort_order.value ? 'asc' : 'desc'}`).then((res) => {
+    plants.value = res.data
+  })
+}
 
 function prevPage() {
   if (pagination.value > 0) {
@@ -64,9 +70,18 @@ provide("pagination", pagination)
   <NavBar/>
   <div class="main">
     <h1>MY PLANTS</h1>
-    <div class="button-container">
-      <button id="list-toggle" @click="toggleView">{{ listview ? 'Show Table' : 'Show Cards' }}</button>
-      <button @click="goToNewPlant">Add Plant</button>
+    <div class="list-navigation-row">
+      <div class="button-container">
+        <div class="checkbox-w-label">
+          <label for="asc">{{ sort_order ? "Ascending" : "Descending" }}</label>
+          <input type="checkbox" v-model="sort_order">
+        </div>
+        <button @click="refreshPlants">Refresh data</button>
+      </div>
+      <div class="button-container">
+        <button id="list-toggle" @click="toggleView">{{ listview ? 'Show Table' : 'Show Cards' }}</button>
+        <button @click="goToNewPlant">Add Plant</button>
+      </div>
     </div>
     <template v-if="listview">
       <PlantCard/>
@@ -103,19 +118,30 @@ provide("pagination", pagination)
 //border: #2c3e50 1px solid; color: #161f13; font-family: "Montserrat", sans-serif; font-size: 24px;
 }
 
-.buttons-row {
+.list-navigation-row {
+  border: 1px solid red;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  width: 80%;
+  margin-bottom: 24px;
 }
 
 .button-container {
-  margin-left: auto;
-  margin-right: 10%;
   display: flex;
   flex-direction: row;
-  column-gap: 12px;
+  align-items: center;
+  column-gap: 16px;
+  border: 1px solid red;
 }
+
+.checkbox-w-label {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 4px;
+}
+
 
 .pagination {
   margin: 48px auto;
